fc81590600b2e40ddeee63428c6549d5e3969717
mordochi <lilynnchi@gmail.com>
2018-03-28 01:36:49 +0800

    add destroy action in TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+  before_action :set_spreadsheet, only:[:create, :destroy]
+
   def create
     @spreadsheet = Spreadsheet.find(params[:spreadsheet_id])
     @tag = @spreadsheet.tags.build(tag_params)
@@ -6,7 +8,18 @@ class TagsController < ApplicationController
     redirect_to spreadsheet_path(@spreadsheet)
   end
 
+  def destroy
+    @spreadsheet = Spreadsheet.find(params[:spreadsheet_id])
+    @tag = Tag.find(params[:id])
+    @tag.destroy #可以加個確認訊息
+    redirect_to spreadsheet_path(@spreadsheet)
+  end
+
   private
+
+  def set_spreadsheet
+    @spreadsheet = Spreadsheet.find(params[:spreadsheet_id])
+  end
 
   def tag_params
     params.require(:tag).permit(:name, :tab, :col)
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   validates_presence_of :name
+  #先只有設name，在想tab跟col是不是可以用我們自己的設定連結起來，不用讓使用者輸入
 end
