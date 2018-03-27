098046f430039ef16505f82efee620f019915320
mordochi <lilynnchi@gmail.com>
2018-03-28 00:41:46 +0800

    set routes for Tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :spreadsheets, only: [:new, :create, :show]
-
+  resources :spreadsheets, only: [:new, :create, :show] do
+    resources :tags, only: [:create, :destroy]
+  end
+  
   root "spreadsheets#new"
 end
