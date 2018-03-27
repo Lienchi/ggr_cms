de08b2014e757460fd878ca3cf04a8135afcaf3a
mordochi <lilynnchi@gmail.com>
2018-03-28 01:14:44 +0800

    add create action in TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,14 @@
 class TagsController < ApplicationController
+  def create
+    @spreadsheet = Spreadsheet.find(params[:spreadsheet_id])
+    @tag = @spreadsheet.tags.build(tag_params)
+    @tag.save!
+    redirect_to spreadsheet_path(@spreadsheet)
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, :tab, :col)
+  end
 end
