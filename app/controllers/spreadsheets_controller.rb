2af404fcc8b6bbf28a95f5395a646d0a73286505
mordochi <lilynnchi@gmail.com>
2018-03-28 00:35:28 +0800

    add create action in SpreadsheetsController

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -2,4 +2,21 @@ class SpreadsheetsController < ApplicationController
   def new
     @spreadsheet = Spreadsheet.new
   end
+
+  def create
+    @spreadsheet = Spreadsheet.new(spreadsheet_params)
+    @spreadsheet.spreadsheet_id =  @spreadsheet.spreadsheet_id.split('/')[5]
+    if @spreadsheet.save
+      redirect_to spreadsheet_path(@spreadsheet)
+    else
+      #add restriction for unvailid url 
+      render :new
+    end
+  end
+
+  private
+
+  def spreadsheet_params
+    params.require(:spreadsheet).permit(:spreadsheet_id)
+  end
 end
