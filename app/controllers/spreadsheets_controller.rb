7810b4c273687903f4135cfd9e881bf440412e8a
mordochi <lilynnchi@gmail.com>
2018-03-28 00:50:30 +0800

    add show action in SpreadsheetsController

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -1,4 +1,8 @@
 class SpreadsheetsController < ApplicationController
+  before_action :set_api, only: [:show]
+  before_action :set_spreadsheet, only: [:show]
+  
+
   def new
     @spreadsheet = Spreadsheet.new
   end
@@ -14,7 +18,24 @@ class SpreadsheetsController < ApplicationController
     end
   end
 
+  def show
+    @responses = @service.get_spreadsheet(@spreadsheet.spreadsheet_id)
+    @tabs = @service.get_spreadsheet(@spreadsheet.spreadsheet_id, fields: "sheets.properties").sheets
+    #讀tab名稱col名稱
+  end
+
   private
+
+  def set_api
+    require 'google/apis/sheets_v4'
+    @service = Google::Apis::SheetsV4::SheetsService.new
+    @service.key = 'AIzaSyCw1eTY-S9Xuxqv4AZ_bfHDlxEJ3KsLuig'
+    @service.authorization = nil
+  end
+
+  def set_spreadsheet
+    @spreadsheet = Spreadsheet.find(params[:id])    
+  end
 
   def spreadsheet_params
     params.require(:spreadsheet).permit(:spreadsheet_id)
