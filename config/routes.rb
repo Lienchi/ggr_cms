5f9f32c17ff7e2d194daa74f1930869799ccb014
mordochi <lilynnchi@gmail.com>
2018-03-28 00:21:26 +0800

    add new action in SpreadsheetsController and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :spreadsheets, only: [:new, :create, :show]
+
+  root "spreadsheets#new"
 end
