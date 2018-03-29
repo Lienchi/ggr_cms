b96e92e4a17377dee0ba73e29e41b163146b5ba9
mordochi <lilynnchi@gmail.com>
2018-03-30 00:21:16 +0800

    set route for spreadsheets#js

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :spreadsheets, only: [:new, :create, :show] do
     resources :tags, only: [:create, :destroy, :edit, :update]
+
+    member do
+      get :js
+    end
   end
   
   root "spreadsheets#new"
