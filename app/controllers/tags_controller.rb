53a0adbee820d9ddf6e1bf26e9b9f84f9019131d
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-31 02:28:51 +0800

    build update action for tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,10 @@
 class TagsController < ApplicationController
-  before_action :set_spreadsheet, only:[:create, :destroy]
   before_action :set_tag, only:[:update, :destroy]
 
-  def create
-    @tag = @spreadsheet.tags.build(tag_params)
-    if @category.save
-      flash[:notice] = "tag creates successfully"
-      redirect_to spreadsheet_path(@spreadsheet)
+  def update
+    if @tag.update(tag_params)
+      redirect_to spreadsheet_path(@tag.tab.spreadsheet.id)
+      flash[:notice] = "tag was successfully updated"
     else
       @spreadsheets = Spreadsheet.all
       @tags = Tag.all
@@ -14,34 +12,14 @@ class TagsController < ApplicationController
     end
   end
 
-  def update
-  if @tag.update(tag_params)
-    redirect_to spreadsheet_path(@spreadsheet)
-    flash[:notice] = "tag was successfully updated"
-  else
-    @spreadsheets = Spreadsheet.all
-    @tags = Tag.all
-    render :spreadsheet_show
-  end
-end
-
-  def destroy
-    @tag.destroy
-    flash[:alert] = "tag was successfully deleted"
-    redirect_to spreadsheet_path(@spreadsheet)
-  end
 
   private
-
-  def set_spreadsheet
-    @spreadsheet = Spreadsheet.find(params[:spreadsheet_id])
-  end
 
   def set_tag
     @tag = Tag.find(params[:id])
   end
 
   def tag_params
-    params.require(:tag).permit(:name, :tab, :col)
+    params.require(:tag).permit(:name, :id)
   end
 end
