b26962b6378221cb9749e4b0d35c2024cf75e828
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-31 02:24:18 +0800

    adjust tags routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :spreadsheets, only: [:new, :create, :show] do
-    resources :tags, only: [:create, :destroy, :edit, :update]
+#    resources :tags, only: [:create, :destroy, :edit, :update]
 
     member do
       get :js
     end
+    
   end
-  
+  resources :tags, only: [:update]
+
   root "spreadsheets#new"
 end
