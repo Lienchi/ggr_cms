d2f6367a9676de3e7a4316dd6ca793486c655e16
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-07 05:31:17 +0800

    set category associations

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,4 +1,4 @@
 class Tab < ApplicationRecord
   belongs_to :spreadsheet
-  has_many :tags
+  has_many :tags 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :tab
+  belongs_to :category 
   #validates_presence_of :name
   #先只有設name，在想tab跟col是不是可以用我們自己的設定連結起來，不用讓使用者輸入
 end
