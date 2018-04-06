7d06766e5582b1f06506a259e9d82d4e4457a5ab
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-07 04:27:57 +0800

    build category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+cateogry_list = [
+  { name: "文字" },
+  { name: "圖片" },
+  { name: "連結" }
+]
+
+cateogry_list.each do |category|
+  Category.create( name: category[:name])
+end
+
+puts "Category created"
