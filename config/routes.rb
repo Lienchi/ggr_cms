f5104f296a98a40c996c70b3642277d523b79f23
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-07 16:38:46 +0800

    add dimension route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :spreadsheets, only: [:new, :create, :show] do
-#    resources :tags, only: [:create, :destroy, :edit, :update]
-
     member do
       get :js
-    end
-    
+    end    
   end
+
+  resources :tabs, only: [] do
+    member do
+      get :dimension
+    end    
+  end
+
+
   resources :tags, only: [:update]
 
   root "spreadsheets#new"
