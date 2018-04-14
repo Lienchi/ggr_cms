ac8ab3f145caacb0ba90352d4889de3dd0b4fd56
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-14 15:39:45 +0800

    build create action for tabs

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,37 +1,43 @@
 class TabsController < ApplicationController
-    require 'google/apis/sheets_v4'
-    before_action :set_api, only: [:dimension]
+  require 'google/apis/sheets_v4'
+  before_action :set_api 
 
-  def dimension 
-    @tab = Tab.find(params[:id])
-    @tab.toggle!(:dimension)
-    @spreadsheet = @tab.spreadsheet
-    @tab.tags.clear
-    if @tab.dimension
-      range = ("A".."ZZ").to_a
-      n = 0
-      @service.get_spreadsheet_values(@spreadsheet.name, @tab.name ).values[0].each do |column|
-        col = @tab.tags.build(tab_name: @tab.name, col: column, spreadsheet_id: @tab.spreadsheet.name, col_range: range[n], category_id: 1)
-        col.save!
-        n += 1
-      end  
-    else
-      range = ("1".."1000").to_a
-      n = 0
-      @service.get_spreadsheet_values(@spreadsheet.name, @tab.name, major_dimension: "COLUMNS" ).values[0].each do |column|
-        col = @tab.tags.build(tab_name: @tab.name, col: column, spreadsheet_id: @tab.spreadsheet.name, col_range: range[n], category_id: 1)
-        col.save!
-        n += 1
-      end
+  def create
+    session["spreadsheet_id"] = params[:spreadsheet][:spreadsheet_url]
+    session["tabs"] = []
+    @service.get_spreadsheet(session["spreadsheet_id"], fields: "sheets.properties").sheets.each_with_index do |tab, index|
+      session["tabs"] << { index: index, name: tab.properties.title } 
     end
-
-    redirect_to spreadsheet_path(@tab.spreadsheet)
   end
+
+  # def dimension 
+  #   @tab = Tab.find(params[:id])
+  #   @tab.toggle!(:dimension)
+  #   @spreadsheet = @tab.spreadsheet
+  #   @tab.tags.clear
+  #   if @tab.dimension
+  #     range = ("A".."ZZ").to_a
+  #     n = 0
+  #     @service.get_spreadsheet_values(@spreadsheet.name, @tab.name ).values[0].each do |column|
+  #       col = @tab.tags.build(tab_name: @tab.name, col: column, spreadsheet_id: @tab.spreadsheet.name, col_range: range[n], category_id: 1)
+  #       col.save!
+  #       n += 1
+  #     end  
+  #   else
+  #     range = ("1".."1000").to_a
+  #     n = 0
+  #     @service.get_spreadsheet_values(@spreadsheet.name, @tab.name, major_dimension: "COLUMNS" ).values[0].each do |column|
+  #       col = @tab.tags.build(tab_name: @tab.name, col: column, spreadsheet_id: @tab.spreadsheet.name, col_range: range[n], category_id: 1)
+  #       col.save!
+  #       n += 1
+  #     end
+  #   end
+
+  #   redirect_to spreadsheet_path(@tab.spreadsheet)
+  # end
+
 
   private
-  def tab_params
-    params.require(:tab).permit(:dimension)
-  end
 
   def set_api
     @service = Google::Apis::SheetsV4::SheetsService.new
