80e65bdc46eaf21d82a8019fb778902568994b5f
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-16 01:50:34 +0800

    build dimension function

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -7,17 +7,36 @@ class TabsController < ApplicationController
     session["spreadsheet_id"] = params[:spreadsheet][:spreadsheet_url]
     session["tabs"] = []
     @service.get_spreadsheet(session["spreadsheet_id"], fields: "sheets.properties").sheets.each_with_index do |tab, index|
-      session["tabs"] << { index: index, name: tab.properties.title } 
+      session["tabs"] << { index: index, name: tab.properties.title, dimension: "ROWS" } 
     end
   end
  
   def hide
- 
     @tab_index = params[:tab_index]
-    #render plain: @hide
   end
 
 
+  def dimension
+    @tab_index = params[:tab_index]
+    @columns = []
+    if session["tabs"][@tab_index.to_i]["dimension"] == "ROWS"
+      session["tabs"][@tab_index.to_i]["dimension"]  = "COLUMNS"
+      range = ("1".."1000").to_a
+      @service.get_spreadsheet_values(session["spreadsheet_id"], session["tabs"][@tab_index.to_i]["name"], major_dimension: "COLUMNS").values[0].each_with_index do |column, index|  
+        @columns << { index: index, name: column, category_id: 1, xml_name: nil, tab_id: @tab_index, range: range[index] }
+      puts "COLUMNS"
+      end
+    else
+      session["tabs"][@tab_index.to_i]["dimension"]  = "ROWS"
+      range = ("A".."ZZ").to_a
+      @service.get_spreadsheet_values(session["spreadsheet_id"], session["tabs"][@tab_index.to_i]["name"]).values[0].each_with_index do |column, index|
+        @columns << { index: index, name: column, category_id: 1, xml_name: nil, tab_id: @tab_index, range: range[index] }
+      puts "ROWS"
+      end
+    end  
+  end
+ 
+ 
   # def dimension 
   #   @tab = Tab.find(params[:id])
   #   @tab.toggle!(:dimension)
