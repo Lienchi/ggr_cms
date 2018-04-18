0d5b8b1e7ab04f30fcab4f46cf6d152ca61a4d2c
Unknown <thetasteofleavesisgreat@gmail.com>
2018-04-19 00:34:33 +0800

    delete useless codes

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -50,33 +50,6 @@ class SpreadsheetsController < ApplicationController
 
   end
 
-
- 
-  # def show
-  #   @tabs = @spreadsheet.tabs
-
-  #   #  @spreadsheed.name,有所有tags.col的陣列,有所有tags.name的陣列
-  #   output_arr = []
-  #   @col_arr = []
-  #   @name_arr = []
-  #   @category_arr = []
-  #   @spreadsheet.tabs.each do |tab|
-  #     tab.tags.each do |tag|
-  #       unless tag.name.blank?
-  #         output_arr << tag
-  #       end  
-  #     end 
-  #   end
-  #   output_arr.each do |x|
-  #     @col_arr << x.tab.name + "!" + x.col_range+":"+ x.col_range
-  #     @name_arr << x.name
-  #     @category_arr << x.category_id
-  #   end
-  # end
-
-
-
-
   private
 
   def set_api
@@ -84,48 +57,6 @@ class SpreadsheetsController < ApplicationController
     @service.key = Rails.application.config_for(:api)["api_key"]
     @service.authorization = nil
   end
-
-  def set_spreadsheet
-    @spreadsheet = Spreadsheet.find(params[:id])    
-  end
-
-  def spreadsheet_params
-    params.require(:spreadsheet).permit(:name)
-  end
-
-  def build_by_google_spreadsheet(spreadsheet)
-    unless spreadsheet.name.blank?
-      url_arr = spreadsheet.name.split('/')
-      if (url_arr.length > 5) && 
-        (url_arr[0]+url_arr[1]+url_arr[2]+url_arr[3]+url_arr[4] == "https:docs.google.comspreadsheetsd") &&
-        (url_arr[5].length == 44 )
-        @spreadsheet.name =  url_arr[5]
-        @spreadsheet.save
-        tabs = @service.get_spreadsheet(spreadsheet.name, fields: "sheets.properties").sheets
-        #save tabs 
-        tabs.each do |tab|
-          tab  = spreadsheet.tabs.build(name: tab.properties.title)
-          tab.save
-          #save tags
-          range = ("A".."ZZ").to_a
-          n = 0
-          @service.get_spreadsheet_values(spreadsheet.name, tab.name).values[0].each do |column|
-            col = tab.tags.build(tab_name: tab.name, col: column, spreadsheet_id: tab.spreadsheet.name, col_range: range[n], category_id: 1)
-            col.save
-            n += 1
-          end
-        end
-        redirect_to spreadsheet_path(spreadsheet)
-      else  
-        flash[:alert] = "Please enter valid Google Spreadsheet"
-        redirect_to new_spreadsheet_path
-      end
-    else
-      flash[:alert] = "Spreadsheet can't be blank"
-      redirect_to new_spreadsheet_path      
-    end    
-  end
-
 
 
 end
